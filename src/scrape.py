763b80c9d6f8e883ba6b0688b97c89ef76d8708e
Duytan <duynttran@yahoo.com>
2021-05-09 17:27:22 -0400

    scrape_gamestop implementation

--- src/scrape.py
+++ src/scrape.py
@@ -20,3 +20,21 @@ def scrape_amazon(search_term):
      # Return price and link dict
     price_link = {"price":price, "link":first_result}
     return price_link
+
+def scrape_gamestop(search_term):
+    '''Performs a search on GameStop given a search term and returns 
+    the price of the first item along with a link'''
+    # Input search
+    browser.open("https://www.gamestop.com")
+    browser.select_form(name="simpleSearch")
+    browser["q"] = search_term
+    result_page = browser.submit()
+    # Get first result price and then open first result
+    soup = BeautifulSoup(result_page, "html.parser")
+    product_grid_soup = soup.find(attrs={"class":"product-grid-redesign"})
+    price = product_grid_soup.find(attrs={"class":"actual-price"}).text
+    first_result = "https://www.gamestop.com" + soup.find(attrs={"class":"product-tile-link"})["href"]
+    browser.open(first_result)
+     # Return price and link dict
+    price_link = {"price":price.strip(), "link":first_result}
+    return price_link
