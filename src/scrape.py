630460b8e88d3999c11e681aaaedb6eba45094de
Duytan <duynttran@yahoo.com>
2021-05-08 21:16:20 -0400

    implemented scrape_amazon

--- src/scrape.py
+++ src/scrape.py
@@ -1,8 +1,22 @@
 import mechanize
+from bs4 import BeautifulSoup
 browser = mechanize.Browser()
-browser.addheaders = [('User-agent', 'FireFoxBrowser')]
-browser.open("https://www.amazon.com")
-browser.select_form(name="site-search")
-browser["field-keywords"] = "spiderman ps4 game of the year edition"
-search_result_page = browser.submit()
-print(search_result_page)
+browser.set_handle_robots(False)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+def scrape_amazon(search_term):
+    '''Performs a search on Amazon given a search term and returns 
+    the price of the first item along with a link'''
+    # Input search
+    browser.open("https://www.amazon.com")
+    browser.select_form(name="site-search")
+    browser["field-keywords"] = search_term
+    result_page = browser.submit()
+    # Get first result price and then open first result
+    soup = BeautifulSoup(result_page, "html.parser")
+    price = soup.find(attrs={"class":"a-offscreen"}).text
+    first_result = "https://www.amazon.com" + soup.find(attrs={"class":"a-link-normal a-text-normal"})["href"]
+    browser.open(first_result)
+     # Return price and link dict
+    price_link = {"price":price, "link":first_result}
+    return price_link
