c295fd18bc505c698c43d6837566bacd589b80a6
Duytan <duynttran@yahoo.com>
2021-05-09 16:51:25 -0400

    modify searches to match console type selection

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,33 +1,45 @@
 // Hide loading indicator upon landing and setup search button
+const BASEURL = "http://127.0.0.1:5000";
 let loading_element = document.getElementById("loading");
 loading_element.style.display = "none";
 let search_button_element = document.getElementById("search_button");
 search_button_element.addEventListener("click", scrape);
-const BASEURL = "http://127.0.0.1:5000";
 
 // Initiates price scrape when search button is clicked
 async function scrape(){
     let search_text = document.getElementById("search_input").value;
     if(search_text.length == 0) return;
-    loading_element.style.display = "block";
+    search_text = reformatSearch(search_text);
+    loading_element.style.display = "block"; //Display loading element
     let results = [];
     results[0] = await initiateAmazonScrape(search_text);
     populateTable(results);
-    loading_element.style.display = "none";
+    loading_element.style.display = "none"; //Hide loading element
 }
 
-// Invokes Amazon backend scraping and reformats return data
+// Reformats search to be more specific and match console
+function reformatSearch(search_text){
+    let console_type = document.getElementById("type_select").value;
+    let search = search_text + " " + console_type + " game";
+    return search;
+}
+
+// Reformats data to be in Vendor, Price, Link object property form
+function reformatData(data, vendor){
+    let price = data.price.toString();
+    let link = data.link.toString();
+    return {"Vendor":vendor, "Price":price, "Link":link};
+}
+
+// Invokes Amazon backend scraping
 async function initiateAmazonScrape(search_text){
     let data = await fetch(BASEURL + "/amazon?term=" + search_text);
     try {
         data = await data.json();
-        let price = data.price.toString();
-        let link = data.link.toString();
-        data = {"Vendor":"Amazon", "Price":price, "Link":link};
     } catch (e) {
         return {"Vendor":"Amazon", "Price":"~", "Link":"~"};
     }
-    return data;
+    return reformatData(data, "Amazon");
 }
 
 // Populates the results table with given array of objects formatted
@@ -43,10 +55,14 @@ function populateTable(results){
         let price = row.insertCell(1);
         price.innerHTML = results[i]["Price"];
         let link = row.insertCell(2);
-        let link_element = document.createElement("a");
-        link_element.href = results[i]["Link"];
-        link_element.innerHTML = "Link";
-        link.appendChild(link_element);
+        if(results[i]["Link"].length > 1){
+            let link_element = document.createElement("a");
+            link_element.href = results[i]["Link"];
+            link_element.innerHTML = "Link";
+            link.appendChild(link_element);
+        } else {
+            link.innerHTML = results[i]["Link"];
+        }
     }
     current_table.parentNode.replaceChild(new_table, current_table)
 }
